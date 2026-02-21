cc7e6865b103171d67a41d19ff524322d45ebf54
contacthassaniomar-spec <contact.hassaniomar@gmail.com>
2026-02-22 00:17:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,6 +165,7 @@ async def callbacks(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elif data == "halal":
     await q.edit_message_text(HALAL_BROW_INFO, reply_markup=main_menu())
+    
     elif data == "categories":
         await q.edit_message_text("Choisis une catégorie 👇", reply_markup=categories_menu())
 
